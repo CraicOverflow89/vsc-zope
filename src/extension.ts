50405e9922dd9e01c435143f36716399e53555e1
Jamie Purchase <jamie.purchase@scholarpack.com>
2020-07-06 16:53:26 +0100

    Initial logic for lookup command.

--- src/extension.ts
+++ src/extension.ts
@@ -15,13 +15,23 @@ export function activate(context: ExtensionContext) {
 	// NOTE: will need to create project initially and also change each time the workspace changes
 	if(project != null) console.log(`data = ${project.toString()}`);
 
-	// PLACEHOLDER COMMAND
+	// Command: Register
 	context.subscriptions.push(commands.registerCommand('zope.register', () => {
 		if(project != null) return;
+		// NOTE: might want to issue warning that this directory is already registered
 		window.showInformationMessage('Registered this directory as a Zope project!');
 		console.log('Project registered');
 		project = Project.create(context);
 		console.log(`data = ${project.toString()}`);
+	}));
+
+	// Command: Lookup
+	context.subscriptions.push(commands.registerCommand('zope.lookup', () => {
+		if(project == null) return;
+		// NOTE: might want to issue warning that command will not work outside of zope project
+		console.log('Lookup routine');
+		// NOTE: need to operate this on current selection
+		//       could be appended to existing Peek Definition stuff or done separately?
 	}));
 }
 
