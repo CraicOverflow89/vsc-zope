a1e92972f171183b8aa42d2a68be67e8f4d07878
Jamie Purchase <jamie.purchase@scholarpack.com>
2020-07-09 17:26:55 +0100

    Document listener that can identify Python script.

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,12 @@
-import { commands, ExtensionContext, window, languages } from 'vscode';
-//import * as nls from 'vscode-nls';
-import { LookupProvider } from './lookup';
-import { Project } from './project';
+import { commands, ExtensionContext, languages, TextEditor, window } from 'vscode'
+//import * as nls from 'vscode-nls'
+import { LookupProvider } from './lookup'
+import { Project } from './project'
 
 export function activate(context: ExtensionContext) {
 
 	// TEMP DEBUG
-	console.log('Zope Extension is now active.');
+	console.log('Zope Extension is now active.')
 
 	// TEST NLS
 	//let localize = nls.config({locale: 'en-GB'})();
@@ -17,12 +17,12 @@ export function activate(context: ExtensionContext) {
 	// NOTE: need to make this accessible to the viewsWelcome when conditions
 
 	// TEST PROJECT
-	let project = Project.exists(context) ? new Project(context) : null;
+	let project = Project.exists(context) ? new Project(context) : null
 	// NOTE: will need to create project initially and also change each time the workspace changes
-	if(project != null) console.log(`data = ${project.toString()}`);
+	if(project != null) console.log(`data = ${project.toString()}`)
 
 	// Active Status
-	commands.executeCommand('setContext', 'zope-active', project != null);
+	commands.executeCommand('setContext', 'zope-active', project != null)
 	// NOTE: this seems too late (but it's not saying it doesn't exist at all) when it checks :/
 
 	// Command: Register
@@ -31,12 +31,12 @@ export function activate(context: ExtensionContext) {
 			window.showErrorMessage('This directory has already been registered as a Zope project!')
 			return;
 		}
-		window.showInformationMessage('Registered this directory as a Zope project!');
-		console.log('Project registered');
-		project = Project.create(context);
-		commands.executeCommand('setContext', 'zope-active', true);
-		console.log(`data = ${project.toString()}`);
-	}));
+		window.showInformationMessage('Registered this directory as a Zope project!')
+		console.log('Project registered')
+		project = Project.create(context)
+		commands.executeCommand('setContext', 'zope-active', true)
+		console.log(`data = ${project.toString()}`)
+	}))
 
 	// Command: Lookup
 	context.subscriptions.push(commands.registerCommand('zope.lookup', () => {
@@ -46,13 +46,27 @@ export function activate(context: ExtensionContext) {
 			// NOTE: could make use of %view.workbench.zope-view.unregistered-folder%
 			return;
 		}
-		console.log('Lookup routine');
+		console.log('Lookup routine')
 		// NOTE: need to operate this on current selection
 		//       could be appended to existing Peek Definition stuff or done separately?
-	}));
+	}))
 
 	// Definition: Lookup
-	languages.registerDefinitionProvider({scheme: 'file', language: 'typescript'}, new LookupProvider());
+	languages.registerDefinitionProvider({scheme: 'file', language: 'typescript'}, new LookupProvider())
+
+	// Listener: Document Change
+	window.onDidChangeVisibleTextEditors((editorList: TextEditor[]) => {
+		editorList.forEach((it: TextEditor) => {
+			const doc = it.document
+			// NOTE: need to exit early if this is not a new document
+			if(doc.isUntitled || !doc.lineCount || doc.languageId != "plaintext") return;
+			if(doc.lineAt(0).text.startsWith("## Script (Python)")) {
+				console.log("this document is a Python script!!")
+				//doc.languageId = "python"
+				// NOTE: this is read-only
+			}
+		})
+	})
 }
 
 export function deactivate() {}
